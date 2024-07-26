e7e934488b9415d636cc840e8f3f489bee4d598b
AMGureev <amgureevhse@gmail.com>
2024-07-26 13:26:26 +0300

    Добавлен запрос для бдшки (3)

--- src/main/java/com/example/demo/data/repository/AssessmentRepository.java
+++ src/main/java/com/example/demo/data/repository/AssessmentRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.data.repository;
 
 import com.example.demo.data.model.Assessment;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface AssessmentRepository extends CrudRepository<Assessment, Integer> {
     List<Assessment> findAssessmentsByChecked(Boolean checked);
+
+    @Query("SELECT a.* FROM assessments a JOIN students s ON a.student_id = s.id " +
+            "WHERE CONCAT(s.name, ' ', s.surname, ' ', s.patronymic) ~ :regexPattern")
+    List<Assessment> findAssessmentsByStudent(String regexPattern);
 }
